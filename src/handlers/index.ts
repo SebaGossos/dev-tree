2612e55239fd0d335d93ac348ca4f604fd70f57c
Sebastian <winigossos@gmail.com>
2025-05-25 08:05:48 -0300

    feat: enhance create account handler with type annotations and error handling

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,22 @@
+import { Request, Response } from "express"
+
 import User from "../models/User"
 
-export const createAccountHandler = async (req, res) => {
+// this is the handler for the create account route
+export const createAccountHandler = async (req: Request, res: Response) => {
+
+  const { email } = req.body;
+  // check if the user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    const {message: error} = new Error('User already exists')
+    res.status(409).send({ error })
+    return
+  }
+  
+  
   const user = new User(req.body)
   await user.save()
   
-
-  res.send('REgistro creado correctamente')
+  res.status(201).send('REgistro creado correctamente')
 }
