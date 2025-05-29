19bac804ed67eb1626f40226cd2eaa9bd08fe447
Sebastian <winigossos@gmail.com>
2025-05-29 08:17:02 -0300

    Add social media icons and implement AuthLayout for authentication views
    - Added LinkedIn, TikTok, Twitch, X, and YouTube SVG icons to the social media section.
    - Created AuthLayout component to wrap authentication views with a consistent layout.
    - Updated router to use AuthLayout for login and registration routes.
    - Modified LoginView to include a navigation link to the registration page.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { LoginView, RegisterView } from './views';
+import AuthLayout from './layouts/AuthLayout';
 
 export default function Router() {
 
   return(
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<AuthLayout />}>
+          {/* Define the routes for the authentication views */}
           <Route path='/auth/login' element={<LoginView />} />
           <Route path='/auth/register' element={<RegisterView />} />
         </Route>
--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,13 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <div>LoginView</div>
+
 
       <nav>
-
         <Link to="/auth/register">¿No tienes cuentasssss? Crea una aquí</Link>
       </nav>
     </>
-  )
+  );
 }
