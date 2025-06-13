4075bd70e9a7a3525a306b33dfbc3e9874dbe3bd
Sebastian <winigossos@gmail.com>
2025-06-13 10:09:20 -0300

    feat: make types for Links

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,19 +1,30 @@
+//? --------- USERS TYPES ----------
 export type User = {
   handle: string;
   name: string;
   email: string;
-  description: string
-  image: string
+  description: string;
+  image: string;
   _id: string;
 };
 
-export type UserForm = Pick<User, "name" | "email" | "handle" > & {
+export type UserForm = Pick<User, "name" | "email" | "handle"> & {
   password: string;
   password_confirmation: string;
 };
 
-export type LoginForm = Pick<User, 'email'> & {
-  password: string
-}
+export type LoginForm = Pick<User, "email"> & {
+  password: string;
+};
 
-export type ProfileForm = Pick<User, 'handle' | 'description' >
+export type ProfileForm = Pick<User, "handle" | "description">;
+
+//? --------- LINKS TYPES ----------
+export type SocialNetwork = {
+  id: number;
+  name: string;
+  url: string;
+  enabled: boolean;
+};
+
+export type DevTreeLink = Pick<SocialNetwork, 'name' | 'url' | 'enabled' >
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
+import { social } from "../data/social"
+
 function LinkTreeView() {
+
+  const [devTreeLinks, setDevTreeLinks] = useState(social)
+  
   return (
     <div>LinkTreeView</div>
   )
