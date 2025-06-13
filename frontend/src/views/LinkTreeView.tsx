7867f230377963b15eae8d287eb2e3e9458e4e5d
Sebastian <winigossos@gmail.com>
2025-06-13 11:51:13 -0300

    feat: add <DevTreeInput /> component with URL handling and integrate with LinkTreeView

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -9,12 +9,19 @@ function LinkTreeView() {
 
   const [devTreeLinks, setDevTreeLinks] = useState(social)
 
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? { ...link, url: e.target.value } : link)
+    console.log(updatedLinks)
+    setDevTreeLinks(updatedLinks)
+  }
+
   return (
     <div className="space-y-5">
       {devTreeLinks.map(item => (
         <DevTreeInput 
           key={item.name}
           item={item} 
+          handleUrlChange={handleUrlChange}
         />
       ))}
     </div>
