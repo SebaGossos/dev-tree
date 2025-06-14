70f2770e0bb51fe218b27895daee375954634b3c
Sebastian <winigossos@gmail.com>
2025-06-14 18:21:37 -0300

    feat(LinkTreeView): validate URL before enabling link when switch btn

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,38 +1,42 @@
 // * External imports
-import { useState } from "react"
+import { useState } from "react";
 
 // * Internal imports
-import { social } from "../data/social"
-import DevTreeInput from "../components/DevTreeInput"
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 function LinkTreeView() {
-
-  const [devTreeLinks, setDevTreeLinks] = useState(social)
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? { ...link, url: e.target.value } : link)
-    console.log(updatedLinks)
-    setDevTreeLinks(updatedLinks)
-  }
+    const updatedLinks = devTreeLinks.map((link) => (link.name === e.target.name ? { ...link, url: e.target.value } : link));
+    setDevTreeLinks(updatedLinks);
+  };
 
   const handleEnableLink = (socialNetwork: string) => {
-    const updatedLinks = devTreeLinks.map(link =>  link.name === socialNetwork ? {...link, enabled: !link.enabled} : link)
-    console.log(updatedLinks)
-    setDevTreeLinks( updatedLinks )
-  }
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          link.enabled = !link.enabled;
+          return link;
+        }
+        toast.error('Invalid URL')
+      }
+      return link;
+    });
+    console.log(updatedLinks);
+    setDevTreeLinks(updatedLinks);
+  };
 
   return (
     <div className="space-y-5">
-      {devTreeLinks.map(item => (
-        <DevTreeInput 
-          key={item.name}
-          item={item} 
-          handleUrlChange={handleUrlChange}
-          handleEnableLink={handleEnableLink}
-        />
+      {devTreeLinks.map((item) => (
+        <DevTreeInput key={item.name} item={item} handleUrlChange={handleUrlChange} handleEnableLink={handleEnableLink} />
       ))}
     </div>
-  )
+  );
 }
 
-export default LinkTreeView
+export default LinkTreeView;
